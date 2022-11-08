72c76eced8bdfd4d4cc6ca9e9b38f5707a6dcb98
mayvastech <mayvaswani@googlemail.com>
2022-11-08 17:45:21 +0000

    Changed container querySelector to elementById

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Grabbed container and text paragraph
-const container = document.querySelector(".container")
-const text = document.getElementById("#text")
+const container = document.getElementById("container");
+const text = document.getElementById("text");
 
 // 3s breathe + 1.5s hold + 3s exhale
-const totalTime = 7500
-const breatheTime = (totalTime / 5) * 2
-const holdTime = totalTime / 5
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
 
-breatheAnimation()
+breatheAnimation();
 function breatheAnimation() {
-    text.innerHTML = "Breathe in"
-    container.className = "container grow"
+  text.textContent = "Breathe in";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.textContent = "Hold";
 
     setTimeout(() => {
-        text.innerText = "Hold"
-
-        setTimeout(() => {
-            text.innerText = "Breathe out"
-            container.className = "container shrink"
-        }, holdTime)
-    }, breatheTime)
+      text.textContent = "Breathe out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
 }
 
-setInterval(breatheAnimation, totalTime)
+setInterval(breatheAnimation, totalTime);
