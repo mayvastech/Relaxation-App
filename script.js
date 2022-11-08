e28a06af2faf837f6b53d927022ba67c0971c3ff
mayvastech <mayvaswani@googlemail.com>
2022-11-08 02:32:43 +0000

    JS added total, breathe + hold time

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Grabbed container and text paragraph
 const container = document.querySelector(".container")
 const text = document.getElementById("#text")
+
+// 3s breathe + 1.5s hold + 3s exhale
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
