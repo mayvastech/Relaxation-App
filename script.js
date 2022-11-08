110dc58fb2978cb4172309f75b428d4afbf05381
mayvastech <mayvaswani@googlemail.com>
2022-11-08 03:10:41 +0000

    Writing breatheAnimation  function using Timeout

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const text = document.getElementById("#text")
 const totalTime = 7500
 const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
+
+function breatheAnimation() {
+    text.innerHTML = "Breathe in"
+    container.className = "container grow"
+
+    setTimeout(() => {
+        text.innerText = "Hold"
+
+        setTimeout(() => {
+            text.innerText = "Breathe out"
+        }, holdTime)
+    }, breatheTime)
+}
